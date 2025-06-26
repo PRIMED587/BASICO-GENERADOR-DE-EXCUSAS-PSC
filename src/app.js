dcda6af18c8ac1e0485c84442f34c80676ecaf4d
Pascual Rivera M <priveram87@gmail.com>
2025-06-26 18:40:16 +0000

    se importa app.js como modulo y definen constantes en la funcion genEx

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ window.onload = () => {
     let cuando = ['Empezando el dia', 'En la noche', 'durante el almuerzo', 'el fin de semana'];
 
     function genEx(quien, accion, objeto, cuando) {
-        numquien = Math.floor(Math.random() * quien.length);
-        numque = Math.floor(Math.random() * accion.length);
-        numobjeto = Math.floor(Math.random() * objeto.length);
-        numcuando = Math.floor(Math.random() * cuando.length);
+       const numquien = Math.floor(Math.random() * quien.length);
+       const  numque = Math.floor(Math.random() * accion.length);
+       const  numobjeto = Math.floor(Math.random() * objeto.length);
+       const  a numcuando = Math.floor(Math.random() * cuando.length);
         return quien[numquien] + " " + accion[numque] + " " + objeto[numobjeto] + " " + cuando[numcuando];
     }
     function cargar() {
