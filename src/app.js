a7e6c3d78943259d545ab3ec963ba6104e142983
Pascual Rivera M <priveram87@gmail.com>
2025-06-26 18:30:49 +0000

    excusahtml fix

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
+window.onload = () => {
 
-let quien = ['Maria', 'Juan', 'Fulano', 'Perencejo', 'Mengano'];
-let accion = ['rompio', 'aplastó', 'rayo', 'ensucio', 'acomodo'];
-let objeto = ['el auto', 'la casa', 'la bici', 'la moto', 'el computador']
-let cuando = ['Empezando el dia', 'En la noche', 'durante el almuerzo', 'el fin de semana'];
+    let quien = ['Maria', 'Juan', 'Fulano', 'Perencejo', 'Mengano'];
+    let accion = ['rompio', 'aplastó', 'rayo', 'ensucio', 'acomodo'];
+    let objeto = ['el auto', 'la casa', 'la bici', 'la moto', 'el computador']
+    let cuando = ['Empezando el dia', 'En la noche', 'durante el almuerzo', 'el fin de semana'];
 
-function genEx(quien, accion, objeto, cuando) {
-    numquien = Math.floor(Math.random() * quien.length);
-    numque = Math.floor(Math.random() * accion.length);
-    numobjeto = Math.floor(Math.random() * objeto.length);
-    numcuando = Math.floor(Math.random() * cuando.length);
-    return quien[numquien] + " " + accion[numque] + " " + objeto[numobjeto] + " " + cuando[numcuando];
+    function genEx(quien, accion, objeto, cuando) {
+        numquien = Math.floor(Math.random() * quien.length);
+        numque = Math.floor(Math.random() * accion.length);
+        numobjeto = Math.floor(Math.random() * objeto.length);
+        numcuando = Math.floor(Math.random() * cuando.length);
+        return quien[numquien] + " " + accion[numque] + " " + objeto[numobjeto] + " " + cuando[numcuando];
+    }
+    function cargar() {
+        var excusahtml = document.getElementById("excusahtml");
+        excusahtml.innerHTML = genEx(quien, accion, objeto, cuando);
+
+    }
+
+    const boton = document.getElementById("generate-excuse")
+    boton.addEventListener("click", cargar)
 }
-function cargar() {
-    var excusahtml = document.getElementById("excusahtml");
-    excusahtml.innerHTML = genEx(quien, accion, objeto, cuando);
-    
-}
 
-cargar();
-console.log(cargar);
+
 
